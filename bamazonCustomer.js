8378b0ba051f59de92b0289ba052266e01b04a0d
YETC7 <tyler_726@live.com>
2017-07-27 04:54:59 -0700

    add purchase option / updates database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,28 +11,80 @@ var connection = mysql.createConnection({
 	database: "bamazon"
 });
 
-var itemList = [];
+var productNames = [];
 
 connection.connect(function(err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-	listItems();
+	listProducts();
 });
 
-function listItems() {
+function listProducts() {
 	connection.query("SELECT * FROM products", function(err, res) {
 		if (err) throw err;
 		// console.log(res);
-		itemList = res;
+		productList = res;
 
 		console.log("\n-----------------------------------")
-		for(var i = 0; i<itemList.length; i++) {
-			console.log("Id:" + itemList[i].item_id +
-				" " + itemList[i].product_name + 
-				" from " + itemList[i].department_name + "\n");
-			console.log("     $" + itemList[i].price + 
-				"  " + itemList[i].stock_quantity + " left\n" + 
+		for(var i = 0; i<productList.length; i++) {
+			productNames.push(productList[i].product_name);
+			console.log("Id:" + productList[i].item_id +
+				" " + productList[i].product_name + 
+				" from " + productList[i].department_name + "\n");
+			console.log("     $" + productList[i].price + 
+				"  " + productList[i].stock_quantity + " left\n" + 
 				"-----------------------------------");
 		}
+
+		var customerQues = [{
+			type:"list",
+			message:"What product do you want to buy?",
+			choices:productNames,
+			name:"productSelected"
+		},{
+			type:"input",
+			message:"How many would you like to purchase?",
+			name:"quantity"
+		}];
+
+		inquirer.prompt(customerQues).then(makePurchase);
 	});
+}
+
+function makePurchase (res) {
+	for(var i = 0; i<productList.length; i++) {
+		if(res.productSelected === productList[i].product_name) {
+			var quantityReq = parseInt(res.quantity);
+			var inStock = parseInt(productList[i].stock_quantity);
+
+			if(quantityReq > inStock) {
+				console.log("Insufficient quantity!");
+				return;
+			}
+
+			inStock-=quantityReq;
+			// console.log(inStock);
+			productList[i].stock_quantity = inStock;
+			console.log("Purchase Successfull");
+			// console.log(productList[i]);
+			return updateStockQuantity(productList[i]);
+		}
+	}
+}
+
+function updateStockQuantity(item) {
+
+	connection.query("UPDATE products SET ? WHERE ?",
+	[{
+		stock_quantity: item.stock_quantity
+    },{
+        item_id: item.item_id
+    }], 
+
+    function(error) {
+    	if (error) throw err;
+    	console.log("Stock Updated");
+    });
+
+    return;
 }
